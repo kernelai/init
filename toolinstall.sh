9d3ad636b8c48ba42f27f3beb511cd7d3ad05c86
coderkiller <redtomato158@gmail.com>
2019-06-19 15:13:13 +0800

    Update toolinstall.sh
    add neofetch

--- toolinstall.sh
+++ toolinstall.sh
@@ -1,2 +1,6 @@
 #!/usr/zsh
 yum install -y htop
+
+# install neofetch a tool display host info
+curl -o /etc/yum.repos.d/konimex-neofetch-epel-7.repo https://copr.fedorainfracloud.org/coprs/konimex/neofetch/repo/epel-7/konimex-neofetch-epel-7.repo
+yum install neofetch
