2067f4eeb1e631ebf72f407b877183432ddd5481
coderkiller <redtomato158@gmail.com>
2019-08-13 15:39:03 +0800

    Update toolinstall.sh
    add a tool to display usage of dirtory

--- toolinstall.sh
+++ toolinstall.sh
@@ -1,5 +1,8 @@
 #!/usr/zsh
+# install htop
 yum install -y htop
+# install ncdu 显示目录大小
+yum install -y ncdu
 
 # install neofetch a tool display host info
 curl -o /etc/yum.repos.d/konimex-neofetch-epel-7.repo https://copr.fedorainfracloud.org/coprs/konimex/neofetch/repo/epel-7/konimex-neofetch-epel-7.repo
