7722cdd7aad712d6ec5d130a6a4505655f7946f4
liuzhen <redtomato158@gmail.com>
2019-09-10 06:01:01 +0000

    add .vimrc

--- toolinstall.sh
+++ toolinstall.sh
@@ -1,9 +1,9 @@
 #!/usr/zsh
 # install htop
-yum install -y htop
+yum install -y htop atop
 # install ncdu 显示目录大小
 yum install -y ncdu
 
 # install neofetch a tool display host info
 curl -o /etc/yum.repos.d/konimex-neofetch-epel-7.repo https://copr.fedorainfracloud.org/coprs/konimex/neofetch/repo/epel-7/konimex-neofetch-epel-7.repo
-yum install neofetch
+yum install -y  neofetch
