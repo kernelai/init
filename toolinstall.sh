a73873f00d511e71d013875aa8a3c6f7e81e6e3b
liuzhen <redtomato158@gmail.com>
2019-09-11 15:47:05 +0000

    add dev tools set mkdir vim basic diratory

--- toolinstall.sh
+++ toolinstall.sh
@@ -7,3 +7,7 @@ yum install -y ncdu
 # install neofetch a tool display host info
 curl -o /etc/yum.repos.d/konimex-neofetch-epel-7.repo https://copr.fedorainfracloud.org/coprs/konimex/neofetch/repo/epel-7/konimex-neofetch-epel-7.repo
 yum install -y  neofetch
+# install dev tools
+yum install centos-release-scl -y
+yum install devtoolset-8 -y
+scl enable devtoolset-8 bash
