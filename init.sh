99601387f098953638d43d84104e0a93b040e926
liuzhen <redtomato158@gmail.com>
2019-09-13 13:24:58 +0000

    add g++ tool

--- init.sh
+++ init.sh
@@ -5,8 +5,12 @@ mkdir -p .repo.bak
 mv ./*.repo .repo.bak 
 curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
 curl -o  /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
+rpm -Uvh http://mirror.ghettoforge.org/distributions/gf/gf-release-latest.gf.el7.noarch.rpm
+rpm --import http://mirror.ghettoforge.org/distributions/gf/RPM-GPG-KEY-gf.el7
 yum makecache
-yum install -y  git vim zsh make wget
+yum -y remove vim-minimal vim-common vim-enhanced 
+yum -y --enablerepo=gf-plus install vim-enhanced 
+yum install -y  git  zsh make wget
 echo "install oh-my-sh"
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 echo "install zsh plugin zsh-autosuggestion"
@@ -18,8 +22,5 @@ git clone https://github.com/rupa/z.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugi
 echo "install fzf"
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
-source ~/.zshrc  
-echo "export LANG=en_US.UTF-8" >> ~/.zshrc
-echo "export LC_ALL=en_US.UTF-8" >> ~/.zshrc
 
-
+cp .zshrc ~/.zshrc
--- toolinstall.sh
+++ toolinstall.sh
@@ -2,12 +2,13 @@
 # install htop
 yum install -y htop atop
 # install ncdu 显示目录大小
-yum install -y ncdu
+yum install -y ncdu figlet
 
 # install neofetch a tool display host info
 curl -o /etc/yum.repos.d/konimex-neofetch-epel-7.repo https://copr.fedorainfracloud.org/coprs/konimex/neofetch/repo/epel-7/konimex-neofetch-epel-7.repo
 yum install -y  neofetch
 # install dev tools
+yum install gcc-c++ -y
 yum install centos-release-scl -y
 yum install devtoolset-8 -y
 scl enable devtoolset-8 bash
